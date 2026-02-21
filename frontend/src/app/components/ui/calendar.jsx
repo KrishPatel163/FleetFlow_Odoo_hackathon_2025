685ca3eabc4ed2ecd2564a2c114df999c367e18c
rensi-padmani <rensi6padmani@gmail.com>
2026-02-21 16:03:31 +0530

    ui commit

--- frontend/src/app/components/ui/calendar.jsx
+++ frontend/src/app/components/ui/calendar.jsx
@@ -1,18 +1,17 @@
 "use client";
 
-import * as React from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { DayPicker } from "react-day-picker";
 
-import { cn } from "./utils";
 import { buttonVariants } from "./button";
+import { cn } from "./utils";
 
 function Calendar({
   className,
   classNames,
   showOutsideDays = true,
   ...props
-}: React.ComponentProps<typeof DayPicker>) {
+}) {
   return (
     <DayPicker
       showOutsideDays={showOutsideDays}
